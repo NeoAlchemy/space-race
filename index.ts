2b87d2bd3995db8061efebf7b5c7a101bf31a37f
NeoAlchemy <NeoAlchemy@gmail.com>
2023-05-01 13:20:20 -0500

    updated space race to use mini game framework

--- index.ts
+++ index.ts
@@ -1,11 +1,189 @@
 // Import stylesheets
 import './style.css';
 
+/* -------------------------------------------------------------------------- */
+/*                                MINI FRAMEWORK.                             */
+/* -------------------------------------------------------------------------- */
+
+// boiler plate setup the canvas for the game
 var canvas = <HTMLCanvasElement>document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 canvas.setAttribute('tabindex', '1');
 canvas.style.outline = 'none';
 canvas.focus();
+
+// utility functions to use everywhere
+class Util {
+  static getRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    // The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+}
+
+// Input Controller to use everywhere
+class InputController {
+  public x: number;
+  public y: number;
+
+  constructor() {}
+
+  update(gameObject: GameObject) {}
+}
+
+class GameObject {
+  public x: number;
+  public y: number;
+  public width: number;
+  public height: number;
+
+  private inputController: InputController;
+
+  constructor(inputController?) {
+    this.inputController = inputController;
+  }
+
+  update() {
+    this.inputController?.update(this);
+  }
+
+  render() {}
+}
+
+class Physics {
+  private gameObjectCollisionRegister: Array<any> = [];
+  private wallCollisionRegister: Array<any> = [];
+  private objectA: GameObject;
+  private objectB: GameObject;
+
+  constructor() {}
+
+  onCollide(
+    objectA: GameObject,
+    objectB: GameObject,
+    callback: Function,
+    scope: any
+  ) {
+    if (objectA && objectB) {
+      this.gameObjectCollisionRegister.push({
+        objectA: objectA,
+        objectB: objectB,
+        callback: callback,
+        scope: scope,
+      });
+    }
+  }
+
+  onCollideWalls(objectA: GameObject, callback: Function, scope: any) {
+    if (objectA) {
+      this.wallCollisionRegister.push({
+        objectA: objectA,
+        callback: callback,
+        scope: scope,
+      });
+    }
+  }
+
+  update() {
+    for (let collisionEntry of this.gameObjectCollisionRegister) {
+      if (
+        collisionEntry.objectA.x > 0 &&
+        collisionEntry.objectA.x < canvas.width &&
+        collisionEntry.objectA.y > 0 &&
+        collisionEntry.objectA.y < canvas.height &&
+        collisionEntry.objectB.x > 0 &&
+        collisionEntry.objectB.x < canvas.width &&
+        collisionEntry.objectB.y > 0 &&
+        collisionEntry.objectB.y < canvas.height &&
+        collisionEntry.objectA.x >= collisionEntry.objectB.x &&
+        collisionEntry.objectA.x <=
+          collisionEntry.objectB.x + collisionEntry.objectB.width &&
+        collisionEntry.objectA.y >= collisionEntry.objectB.y &&
+        collisionEntry.objectA.y <=
+          collisionEntry.objectB.y + collisionEntry.objectB.height
+      ) {
+        collisionEntry.callback.bind(collisionEntry.scope).apply();
+      }
+    }
+    for (let wallCollisionEntry of this.wallCollisionRegister) {
+      if (
+        wallCollisionEntry.objectA.y < wallCollisionEntry.objectA.height ||
+        wallCollisionEntry.objectA.y > canvas.height ||
+        wallCollisionEntry.objectA.x < wallCollisionEntry.objectA.width ||
+        wallCollisionEntry.objectA.x > canvas.width
+      ) {
+        wallCollisionEntry.callback.bind(wallCollisionEntry.scope).apply();
+      }
+    }
+  }
+}
+
+class Scene {
+  public children: Array<any>;
+  public physics: Physics;
+
+  constructor() {
+    this.children = [];
+    this.physics = new Physics();
+  }
+
+  add(gameObject: GameObject) {
+    this.children.push(gameObject);
+  }
+
+  create() {}
+
+  update() {
+    for (let gameObject of this.children) {
+      gameObject.update();
+    }
+    this.physics.update();
+  }
+
+  render() {
+    // update the game background
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = COLOR_BACKGROUND;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    for (let gameObject of this.children) {
+      gameObject.render();
+    }
+  }
+}
+
+class Game {
+  private scene: Scene;
+  private id: number;
+
+  constructor(scene: Scene) {
+    this.scene = scene;
+    this.scene.create();
+    //Setup Components
+    this.id = requestAnimationFrame(this.gameLoop);
+  }
+
+  gameLoop(timestamp) {
+    // WARNING: This pattern is not using Times Step and as such
+    // Entities must be kept low, when needing multiple entities, scenes,
+    // or other components it's recommended to move to a Game Framework
+
+    // game lifecycle events
+    game.scene.update();
+    game.scene.render();
+
+    // call next frame
+    cancelAnimationFrame(game.id);
+    game.id = requestAnimationFrame(game.gameLoop);
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+/*                               GAME SPECIFIC CODE                           */
+/* -------------------------------------------------------------------------- */
+
+/* ------------------------------ GAME MECHANICS ---------------------------- */
 
 const ASTEROID_SIZE = 5;
 const ASTEROID_COUNT = 25;
@@ -18,76 +196,82 @@ const COLOR_BACKGROUND = '#000';
 const COLOR_SCORE = '#FFF';
 const COLOR_ASTEROID = '#FFF';
 
-class Util {
-  static getRandomInt(min: number, max: number) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    // The maximum is inclusive and the minimum is inclusive
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-}
+/* --------------------------------- ENTITIES ------------------------------- */
 
-class Asteroid {
-  public x: number = 0;
-  public y: number = 0;
+class Asteroid extends GameObject {
   public direction: string;
 
-  constructor(x: number, y: number, direction: string) {
+  constructor(direction: string) {
+    super();
+    this.width = ASTEROID_SIZE;
+    this.height = ASTEROID_SIZE;
     this.direction = direction;
-    this.draw(x, y);
-  }
-
-  draw(x: any, y: any) {
-    this.x = x;
-    this.y = y;
-    ctx.fillStyle = COLOR_ASTEROID;
-    ctx.fillRect(x, y, ASTEROID_SIZE, ASTEROID_SIZE);
-  }
-
-  move() {
     if (this.direction == 'RIGHT') {
-      this.draw(this.x + STAR_VELOCITY, this.y);
+      this.x = canvas.width - this.width;
     } else {
-      this.draw(this.x - STAR_VELOCITY, this.y);
+      this.x = 0;
     }
+  }
+
+  update() {
+    super.update();
+
+    if (this.direction == 'RIGHT') {
+      this.x = this.x - STAR_VELOCITY;
+    } else {
+      this.x = this.x + STAR_VELOCITY;
+    }
+  }
+
+  render() {
+    super.render();
+    ctx.fillStyle = COLOR_ASTEROID;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
 
-class AsteroidField {
+class AsteroidField extends GameObject {
   public storage: Array<Asteroid> = [];
 
   constructor() {
-    this.draw();
-  }
-
-  draw() {
-    this.storage = [];
-    for (let i = 0; i < ASTEROID_COUNT; i++) {
+    super();
+    while (this.storage.length < ASTEROID_COUNT) {
       let randomY = Util.getRandomInt(canvas.height + CENTER_LINE_HEIGHT, 0);
-      let randomX = Util.getRandomInt(canvas.width, 0);
+      let randomX = Util.getRandomInt(0, canvas.width);
       let direction = Util.getRandomInt(0, 1) ? 'LEFT' : 'RIGHT';
-      let asteroid = new Asteroid(randomX, randomY, direction);
+      let asteroid = new Asteroid(direction);
+      asteroid.y = randomY;
+      asteroid.x = randomX;
       this.storage.push(asteroid);
     }
   }
 
   update() {
-    if (this.storage.length > 0 && this.storage.length < ASTEROID_COUNT) {
+    super.update();
+    if (this.storage.length < ASTEROID_COUNT) {
       let randomY = Util.getRandomInt(canvas.height + CENTER_LINE_HEIGHT, 0);
+      let randomX = Util.getRandomInt(0, canvas.width);
       let direction = Util.getRandomInt(0, 1) ? 'LEFT' : 'RIGHT';
-      let x = direction == 'RIGHT' ? 0 : canvas.width;
-      let asteroid = new Asteroid(x, randomY, direction);
+      let asteroid = new Asteroid(direction);
+      asteroid.y = randomY;
+      asteroid.x = direction == 'LEFT' ? 0 : canvas.width;
       this.storage.push(asteroid);
     }
-  }
-
-  move() {
     for (let i = 0; i < this.storage.length; i++) {
       let asteroid: Asteroid = this.storage[i];
-      asteroid.move();
+      asteroid.update();
       if (this.isAsteroidOutOfBounds(asteroid)) {
         this.storage.splice(i, 1);
       }
+    }
+  }
+
+  render() {
+    super.render();
+
+    for (let i = 0; i < this.storage.length; i++) {
+      let asteroid: Asteroid = this.storage[i];
+      asteroid.render();
     }
   }
 
@@ -96,25 +280,32 @@ class AsteroidField {
   }
 }
 
-class Ship {
-  public x: number;
-  public y: number;
+class Ship extends GameObject {
   public spaceshipSize: number = 32;
   private spaceship =
     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAHFJREFUOE/FklEKgDAMQ839D91RIdJ1aUEq6JeY5nVxwdU8ZmYuA0A1VgpupjG+Z5AEKEMFOQDdNqVtAGZWuSvt+wi+ffQP/geMb6FrnYr33EK3Obcvzt6AN2bC6EEsCAuUv9Hk8Y75eDxSux5kbVzlBafciA35C1jZAAAAAElFTkSuQmCC';
 
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-    this.create();
+  constructor(_position) {
+    if (_position == 'LEFT') {
+      super(new LeftShipInputController());
+      this.x = canvas.width / 4;
+      this.y = canvas.height - this.spaceshipSize;
+    } else {
+      super(new RightShipInputController());
+      this.x = canvas.width - canvas.width / 4;
+      this.y = canvas.height - this.spaceshipSize;
+    }
+    this.width = this.spaceshipSize;
+    this.height = this.spaceshipSize;
   }
 
-  create() {
-    this.y -= this.spaceshipSize;
-    this.draw();
+  update() {
+    super.update();
   }
 
-  draw() {
+  render() {
+    super.render();
+
     let myImage = new Image();
     myImage.src = this.spaceship;
     ctx.drawImage(
@@ -125,32 +316,27 @@ class Ship {
       this.spaceshipSize
     );
   }
-
-  move(direction: string) {
-    if (direction == 'UP') {
-      this.y -= SHIP_VELOCITY;
-      this.draw();
-    } else {
-      if (this.y + this.spaceshipSize < canvas.height) {
-        this.y += SHIP_VELOCITY;
-        this.draw();
-      }
-    }
-  }
 }
 
-class Score {
+class Score extends GameObject {
   private leftScore: number = 0;
   private rightScore: number = 0;
 
   constructor() {
-    this.display();
+    super();
   }
 
-  display() {
+  update() {
+    super.update();
+  }
+
+  render() {
+    super.render();
+
     let leftPosition = canvas.width / 8;
     let rightPosition = canvas.width - canvas.width / 8;
     ctx.fillStyle = COLOR_SCORE;
+    ctx.font = '48px Verdana';
     ctx.fillText(String(this.leftScore), leftPosition, canvas.height);
     ctx.fillText(String(this.rightScore), rightPosition, canvas.height);
   }
@@ -164,84 +350,115 @@ class Score {
   }
 }
 
-class InputController {
-  private leftShip: Ship;
-  private rightShip: Ship;
+/* ------------------------------- InputController  ----------------------------- */
+
+class RightShipInputController extends InputController {
+  private direction: string;
+
   private keys: Array<boolean> = [];
-  constructor(leftShip: Ship, rightShip: Ship) {
-    this.leftShip = leftShip;
-    this.rightShip = rightShip;
+  constructor() {
+    super();
 
     document.addEventListener(
       'keydown',
       (evt) => {
-        this.keys[evt.key] = true;
-
-        if (this.keys['ArrowUp']) {
-          this.rightShip.move('UP');
+        if (evt.key == 'ArrowUp') {
+          this.direction = 'UP';
         }
 
-        if (this.keys['ArrowDown']) {
-          this.rightShip.move('DOWN');
-        }
-        if (this.keys['w']) {
-          this.leftShip.move('UP');
-        }
-
-        if (this.keys['s']) {
-          this.leftShip.move('DOWN');
+        if (evt.key == 'ArrowDown') {
+          this.direction = 'DOWN';
         }
 
         evt.preventDefault();
       },
       false
     );
+  }
+
+  update(gameObject: GameObject) {
+    if (this.direction == 'UP') {
+      gameObject.y -= SHIP_VELOCITY;
+    } else if (this.direction == 'DOWN') {
+      if (gameObject.y + gameObject.height < canvas.height) {
+        gameObject.y += SHIP_VELOCITY;
+      }
+    }
+    this.direction = null;
+  }
+}
+
+class LeftShipInputController extends InputController {
+  private direction: string;
+
+  private keys: Array<boolean> = [];
+  constructor() {
+    super();
 
     document.addEventListener(
       'keydown',
       (evt) => {
-        this.keys[evt.key] = false;
+        if (evt.key == 'w') {
+          this.direction = 'UP';
+        }
+
+        if (evt.key == 's') {
+          this.direction = 'DOWN';
+        }
+
+        evt.preventDefault();
       },
       false
     );
   }
+
+  update(gameObject: GameObject) {
+    if (this.direction == 'UP') {
+      gameObject.y -= SHIP_VELOCITY;
+    } else if (this.direction == 'DOWN') {
+      if (gameObject.y + gameObject.height < canvas.height) {
+        gameObject.y += SHIP_VELOCITY;
+      }
+    }
+    this.direction = null;
+  }
 }
 
-class Game {
-  private leftShip: Ship = new Ship(-32, -32);
-  private rightShip: Ship = new Ship(-32, -32);
-  private score: Score = new Score();
+/* --------------------------------- SCENE ------------------------------- */
+class MainLevel extends Scene {
+  private leftShip: Ship;
+  private rightShip: Ship;
+  private score: Score;
   private asteroidField: AsteroidField;
 
   constructor() {
-    //Place Ships Down
-    this.setupLeftShip();
-    this.setupRightShip();
+    super();
+  }
 
-    //Setup Components
-    let inputController = new InputController(this.leftShip, this.rightShip);
+  create() {
+    this.leftShip = new Ship('LEFT');
+    this.add(this.leftShip);
+
+    this.rightShip = new Ship('RIGHT');
+    this.add(this.rightShip);
+
+    this.score = new Score();
+    this.add(this.score);
 
     this.asteroidField = new AsteroidField();
-    requestAnimationFrame(this.gameLoop);
-  }
-  // Setup Ship
-  setupLeftShip() {
-    let leftX = canvas.width / 4;
-    let leftY = canvas.height;
-    this.leftShip = new Ship(leftX, leftY);
+    this.add(this.asteroidField);
+
+    this.physics.onCollideWalls(this.leftShip, this.leftShipHitCeiling, this);
+    this.physics.onCollideWalls(this.rightShip, this.rightShipHitCeiling, this);
   }
 
-  setupRightShip() {
-    let rightX = canvas.width - canvas.width / 4;
-    let rightY = canvas.height;
-    this.rightShip = new Ship(rightX, rightY);
+  update() {
+    super.update();
   }
-  // Setup Game Area
-  setup() {
-    ctx.fillStyle = COLOR_BACKGROUND;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    //dividing line
+  render() {
+    super.render();
+
     ctx.fillStyle = COLOR_CENTER_LINE;
     ctx.fillRect(
       canvas.width / 2,
@@ -249,77 +466,57 @@ class Game {
       CENTER_LINE_WIDTH,
       CENTER_LINE_HEIGHT
     );
-
-    ctx.font = '48px Verdana';
   }
 
-  gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // use game object because of requewstAnimationFrame
-    // calling function with this scope
-    game.setup();
-
-    game.checkCollision();
-
-    game.leftShip.draw();
-    game.rightShip.draw();
-    game.asteroidField.move();
-    game.asteroidField.update();
-    game.score.display();
-    requestAnimationFrame(game.gameLoop);
+  leftShipHitCeiling() {
+    this.score.incrementLeft();
+    this.leftShip.x = canvas.width / 4;
+    this.leftShip.y = canvas.height - this.leftShip.height;
   }
 
-  checkCollision() {
-    game.shipHitCeiling();
-    game.asteroidHitShip();
+  rightShipHitCeiling() {
+    this.score.incrementRight();
+    this.rightShip.x = canvas.width - canvas.width / 4;
+    this.rightShip.y = canvas.height - this.rightShip.height;
   }
+}
 
-  shipHitCeiling() {
-    if (game.leftShip.y < game.leftShip.spaceshipSize) {
-      game.score.incrementLeft();
-      game.leftShip.x = canvas.width / 4;
-      game.leftShip.y = canvas.height;
-      game.leftShip.create();
-    }
-    if (game.rightShip.y < game.rightShip.spaceshipSize) {
-      game.score.incrementRight();
+/*
+asteroidHitShip() {
+  for (let k = 0; k < game.asteroidField.storage.length; k++) {
+    let asteroid = game.asteroidField.storage[k];
+    if (
+      game.rightShip.x != 0 &&
+      game.rightShip.y != 0 &&
+      asteroid.x >= game.rightShip.x &&
+      asteroid.x <= game.rightShip.x + game.rightShip.spaceshipSize &&
+      asteroid.y >= game.rightShip.y &&
+      asteroid.y <= game.rightShip.y + game.rightShip.spaceshipSize
+    ) {
+      game.asteroidField.storage.splice(k, 1);
       game.rightShip.x = canvas.width - canvas.width / 4;
       game.rightShip.y = canvas.height;
       game.rightShip.create();
     }
-  }
-
-  asteroidHitShip() {
-    for (let k = 0; k < game.asteroidField.storage.length; k++) {
-      let asteroid = game.asteroidField.storage[k];
-      if (
-        game.rightShip.x != 0 &&
-        game.rightShip.y != 0 &&
-        asteroid.x >= game.rightShip.x &&
-        asteroid.x <= game.rightShip.x + game.rightShip.spaceshipSize &&
-        asteroid.y >= game.rightShip.y &&
-        asteroid.y <= game.rightShip.y + game.rightShip.spaceshipSize
-      ) {
-        game.asteroidField.storage.splice(k, 1);
-        game.rightShip.x = canvas.width - canvas.width / 4;
-        game.rightShip.y = canvas.height;
-        game.rightShip.create();
-      }
-      if (
-        game.leftShip.x != 0 &&
-        game.leftShip.y != 0 &&
-        asteroid.x >= game.leftShip.x &&
-        asteroid.x <= game.leftShip.x + game.leftShip.spaceshipSize &&
-        asteroid.y >= game.leftShip.y &&
-        asteroid.y <= game.leftShip.y + game.leftShip.spaceshipSize
-      ) {
-        game.asteroidField.storage.splice(k, 1);
-        game.leftShip.x = canvas.width / 4;
-        game.leftShip.y = canvas.height;
-        game.leftShip.create();
-      }
+    if (
+      game.leftShip.x != 0 &&
+      game.leftShip.y != 0 &&
+      asteroid.x >= game.leftShip.x &&
+      asteroid.x <= game.leftShip.x + game.leftShip.spaceshipSize &&
+      asteroid.y >= game.leftShip.y &&
+      asteroid.y <= game.leftShip.y + game.leftShip.spaceshipSize
+    ) {
+      game.asteroidField.storage.splice(k, 1);
+      game.leftShip.x = canvas.width / 4;
+      game.leftShip.y = canvas.height;
+      game.leftShip.create();
     }
   }
 }
+*/
 
-let game = new Game();
+/* -------------------------------------------------------------------------- */
+/*                                RUN GAME.                                   */
+/* -------------------------------------------------------------------------- */
+let mainLevel = new MainLevel();
+let game = new Game(mainLevel);
